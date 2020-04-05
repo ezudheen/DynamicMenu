daad4990666ca7b108cb939a4e99ea021cef3812
ezudheen <ezudheen@gmail.com>
2020-04-05 11:21:04 +0530

    interface declaration

--- src/API/restAPIs/dataBaseAPI/data_base_operations.js
+++ src/API/restAPIs/dataBaseAPI/data_base_operations.js
@@ -1,3 +1,95 @@
+-----------------------Argument--------------------------
+<db_op_arg>
+	<db_name>
+		name of the db
+	</db_name>	
+	<db_operation>
+		create_table/inert_data/...
+	</db_operation>	
+	<table_name>
+		name of the table
+	</table_name>
+	<table_header>
+		<column1>
+			<name_field>
+				name of field1
+			</name_field>
+			<data_type_field>
+				data_type of field1
+			</data_type_field>
+		</column1>
+		<column2>
+			<name_field>
+				name of field2
+			</name_field>
+			<data_type_field>
+				data_type of field2
+			</data_type_field>
+		</column2>
+	</table_header>
+	<table_row>
+		<column1>
+			<value_field>
+				value of field1
+			</value_field>
+		</column1>
+		<column2>
+			<value_field>
+				value of field2
+			</value_field>
+		</column2>
+	</table_row>
+</db_op_arg>
+
+-----------------------Return Value--------------------------
+
+<db_op_ret>
+	<db_name>
+		name of the db
+	</db_name>	
+	<db_operation>
+		create_table/inert_data/...
+	</db_operation>
+	<db_operation_status>
+		successful/failed
+	</db_operation_status>		
+	<table_name>
+		name of the table
+	</table_name>
+	<table_header>
+		<column1>
+			<name_field>
+				name of field1
+			</name_field>
+			<data_type_field>
+				data_type of field1
+			</data_type_field>
+		</column1>
+		<column2>
+			<name_field>
+				name of field2
+			</name_field>
+			<data_type_field>
+				data_type of field2
+			</data_type_field>
+		</column2>
+	</table_header>
+	<table_row>
+		<column1>
+			<value_field>
+				value of field1
+			</value_field>
+		</column1>
+		<column2>
+			<value_field>
+				value of field2
+			</value_field>
+		</column2>
+	</table_row>
+</db_op_ret>
+
+-----------------------Functions--------------------------
+
 Create a table
 
 Delete a table
